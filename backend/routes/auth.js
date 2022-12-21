67a3c9ab51fa1c0d043bf40ccdc46ef8ed94ad04
beingnikil07 <kumarnikil220@gmail.com>
2022-12-21 13:30:35 +0530

    saving user data from request body

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,13 @@
-const express=require('express')  
-const router=express.Router();  
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
 
-router.get('/',(req,res)=>{ 
-   console.log(req.body) 
-   res.send(req.body)   
-})
-module.exports=router;
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body); //User schema ko humne req.body de dii
+  // hum jo bhi req se data send karenge thunderclient ke through
+  //wo mongoDb ke User mai jaayega
+  user.save();    // for saving the data in user 
+  res.send(req.body);
+});
+module.exports = router;
